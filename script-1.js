fdec1caefcc0aa1a6b83fe5f2638094e036bfa4c
FARUK2059 <omarfaruk34552@gmail.com>
2024-02-25 16:08:17 +0600

    JavaScript DOM Basic and HTML

---
+++ script-1.js
@@ -0,0 +1,17 @@
+console.log('first file');
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+const allTitles = document.getElementsByClassName('section-titel');
+console.log(allTitles);
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection);
+secondSection.style.color = 'white';
+secondSection.style.backgroundColor = 'tomato';
+secondSection.style.borderRadius = '15px';
+secondSection.style.padding = ''
+
+// document.querySelectorAll
+// document.querySelector
---
+++ script-3.js
@@ -0,0 +1,17 @@
+console.log('Third file');
+
+const main = document.getElementById('main-container');
+const section = document.createElement('section');
+section.innerHTML = `
+<h1> My dynamic section</h1>
+<p>Extra text add inside paragraph</p>
+<ul>
+    <li> first item </li>
+    <li> first item </li>
+    <li> first item </li>
+    <li> first item </li>
+</ul>
+
+`
+
+main.appendChild(section);
