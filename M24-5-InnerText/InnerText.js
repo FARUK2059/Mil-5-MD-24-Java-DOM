8f69893fabecaef4c5bdd55289bf5652528c96de
FARUK2059 <omarfaruk34552@gmail.com>
2024-02-26 13:29:11 +0600

    JvaScript DOM with innerText Mathod

---
+++ M24-5-InnerText/InnerText.js
@@ -0,0 +1,16 @@
+
+
+function play1() {
+    let text = document.getElementById('h1').innerText;
+    document.getElementById('demo').innerText = text;
+}
+
+function play2() {
+    let text2 = document.getElementById("h2").innerText;
+    document.getElementById("demo").innerText = text2;
+}
+
+function play3() {
+    let text3 = document.getElementById("p").innerText;
+    document.getElementById('demo').innerText = text3;
+}
