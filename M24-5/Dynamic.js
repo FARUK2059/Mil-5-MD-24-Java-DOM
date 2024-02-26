aa0ec1b365130b7588c1ce381058c4a6b94c87df
FARUK2059 <omarfaruk34552@gmail.com>
2024-02-26 12:09:19 +0600

    Dynamic Color changed

---
+++ M24-5/Dynamic.js
@@ -0,0 +1,9 @@
+function changeColor(){
+    const box = document.getElementById('My-Box');
+    box.style.backgroundColor = 'blue';
+    box.style.color = 'white';
+}
+
+function direct() {
+    alert(document.getElementById(alart))
+}
